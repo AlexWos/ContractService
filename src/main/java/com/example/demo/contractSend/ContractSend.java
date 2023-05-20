3a377029e278927a41852d1fde901c85b6d06676
AlexWos <sanek271s15@gmail.com>
2023-05-21 01:11:14 +0500

    0.2

--- src/main/java/com/example/demo/contractSend/ContractSend.java
+++ src/main/java/com/example/demo/contractSend/ContractSend.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ContractSend {
     private final RabbitTemplate rabbitTemplate;
