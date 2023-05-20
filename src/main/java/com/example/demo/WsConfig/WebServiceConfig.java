baa2963fccb0147865774d25b4e3d7e6c1e3cff2
AlexWos <sanek271s15@gmail.com>
2023-05-21 01:56:33 +0500

    0.2

--- src/main/java/com/example/demo/WsConfig/WebServiceConfig.java
+++ src/main/java/com/example/demo/WsConfig/WebServiceConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.WsConfig;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
