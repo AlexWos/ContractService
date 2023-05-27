10d36d4adb5324f4506344d1192b5143ca8bcb6d
AlexWos <sanek271s15@gmail.com>
2023-05-28 03:46:13 +0500

    0.1

--- src/main/java/com/example/demo/ContractServiceEnd.java
+++ src/main/java/com/example/demo/ContractServiceEnd.java
@@ -5,25 +5,18 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import ru.demo.contractservice.CreateNewContractRequest;
-import ru.demo.contractservice.CreateNewContractResponse;
+
 
 @Endpoint
 public class ContractServiceEnd {
 
     private static final String NAMESPACE_URI = "http://demo.ru/ContractService/";
 
-    private ContractService contractService;
+    private ContractServiceStart contractService;
 
     @Autowired
-    public ContractServiceEnd(ContractService contractService) {
+    public ContractServiceEnd(ContractServiceStart contractService) {
         this.contractService = contractService;
     }
-//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getContractRequest")
-//    @ResponsePayload
-//    public CreateNewContractResponse getContract(@RequestPayload CreateNewContractRequest request) {
-//
-//        return ;
-//    }
 
 }
